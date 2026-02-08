2b9c79998b04e4886a07c0173b1b0eb1182ec3f0
pleemeyfrrr18 <karamurzanovarsen@gmail.com>
2026-02-09 00:00:21 +0500

    Update script3.js

--- lab3/Objects: the basics/task2/script3.js
+++ lab3/Objects: the basics/task2/script3.js
@@ -19,3 +19,18 @@ calculator.read();
 
 alert( "Sum=" + calculator.sum() );
 alert( "Mul=" + calculator.mul() );
+
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('How much to add?', 0);
+  };
+
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
