766e595b12dad4e20595ebc8bf208eac63c719c0
pleemeyfrrr18 <karamurzanovarsen@gmail.com>
2026-02-09 11:22:53 +0500

    Update script.js

--- lab3/task3/script.js
+++ lab3/task3/script.js
@@ -1,1 +1,35 @@
+let user = { 
+  name: "John"
+};
+let id = Symbol("id");
+user[id] = 1;
+alert( user[id] );
 
+
+let id = Symbol("id");
+let user = {
+  name: "John",
+  [id]: 123 
+};
+
+
+let id = Symbol("id");
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123
+};
+
+for (let key in user) alert(key); 
+
+alert( "Direct: " + user[id] )
+
+
+let id = Symbol("id");
+let user = {
+  [id]: 123
+};
+
+let clone = Object.assign({}, user);
+
+alert( clone[id] );
